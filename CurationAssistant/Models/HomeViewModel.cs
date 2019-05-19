5089505d060265de069d9624c21d481323933272
lion-200 <i.duz86@outlook.com>
2019-05-19 16:14:00 +0200

    - Default Layout cleaned up - Bootstrap updated to 4.3 - Author details html block added - Author transaction html block added - Post details html block added

---
+++ CurationAssistant/Models/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CurationAssistant.Models
+{
+    public class HomeViewModel
+    {
+        public string PostLink { get; set; }
+
+        public HomeViewModel()
+        {
+            
+        }
+
+    }
+}
